1b560acb2bdef24f4dbd230dd314fb2899bde425
Dova0205 <joaqui.leon@hotmail.es>
2026-04-26 22:24:13 -0300

    Borrado de carpetas duplicadas

--- Dominio/Imagen.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TPWinForm_equipo_10B.Dominio
-{
-    public class Imagen
-    {
-        public int Id { get; set; }
-        public int IdArticulo { get; set; }
-        public string ImagenUrl { get; set; }
-    }
-}
